35eefa2da3a17654cd65d931b28aaa351d54738b
Grealish17 <ark-gto@yandex.ru>
2022-09-23 23:06:23 +0300

    Add invalid_argument exception, namespace LB, constructor

--- dialog_functions.cpp
+++ dialog_functions.cpp
@@ -1,5 +1,7 @@
 #include "lemniscate.h"
 
+using namespace LB;
+
 int dialog(const char *msgs[], int K){
     std::string errmsg;
     int rc;
@@ -43,9 +45,10 @@ int D_Get_Polar_Radius_By_Angle(Lemniscate_Bernoulli& lemniscate){
     std::cout << "Enter angle value in radians: ";
     std::cin >> fi;
     try{
-        std::cout << "Polar radius value is: " << lemniscate.get_polar_radius_by_angle(fi) << "\n";
+        double r = lemniscate.get_polar_radius_by_angle(fi);
+        std::cout << "Polar radius value is: " <<  r << "\n";
     }
-    catch(double fi){
+    catch(std::invalid_argument& exception){
         std::cout << "Lemniscate is defined for the angle [-pi/4; pi/4] U [3*pi/4; 5*pi/4]! Your angle value is: " << fi << "\n";
     }
     return 1;
@@ -55,10 +58,11 @@ int D_Get_Radius_Of_Curvature_By_Polar_Radius(Lemniscate_Bernoulli& lemniscate){
     std::cout << "Enter polar radius value: ";
     std::cin >> r;
     try{
-        std::cout << "Radius of curvature value is: " << lemniscate.get_radius_of_curvature_by_polar_radius(r) << "\n";
+        double R = lemniscate.get_radius_of_curvature_by_polar_radius(r);
+        std::cout << "Radius of curvature value is: " << R << "\n";
     }
-    catch(double r){
-        std::cout << "Polar radius value must be a positive real number! Your radius value is: " << r << "\n";
+    catch(std::invalid_argument& exception){
+        std::cout << "Polar radius value must be a positive real number and less than c*sqrt(2)! Your radius value is: " << r << "\n";
     }
     return 1;
 }
@@ -67,9 +71,10 @@ int D_Get_Radius_Of_Curvature_By_Angle(Lemniscate_Bernoulli& lemniscate){
     std::cout << "Enter angle value in radians: ";
     std::cin >> fi;
     try{
-        std::cout << "Polar radius value is: " << lemniscate.get_radius_of_curvature_by_angle(fi) << "\n";
+        double r = lemniscate.get_radius_of_curvature_by_angle(fi);
+        std::cout << "Polar radius value is: " << r << "\n";
     }
-    catch(double fi){
+    catch(std::invalid_argument& exception){
         std::cout << "Lemniscate is defined for the angle [-pi/4; pi/4] U [3*pi/4; 5*pi/4]! Your angle value is: " << fi << "\n";
     }
     return 1;
@@ -79,9 +84,10 @@ int D_Get_Sector_Area_By_Angle(Lemniscate_Bernoulli& lemniscate){
     std::cout << "Enter angle value in radians: ";
     std::cin >> alpha;
     try{
-        std::cout << "Sector area is: " << lemniscate.get_sector_area_by_angle(alpha) << "\n";
+        double area = lemniscate.get_sector_area_by_angle(alpha);
+        std::cout << "Sector area is: " << area << "\n";
     }
-    catch(double alpha){
+    catch(std::invalid_argument& exception){
         std::cout << "Sector area is defined for the angle [0; pi/4]! Your angle value is: " << alpha << "\n";
     }
     return 1;
--- lemniscate.h
+++ lemniscate.h
@@ -3,54 +3,53 @@
 #include <cstring>
 #define PI 3.14159265
 
-class Lemniscate_Bernoulli{
-private:
-    double c = 1.0;
-public:
-    void set_data(double data_c){
-        c = data_c;
-    }
-    [[nodiscard]]
-    double get_data() const{
-        return c;
-    }
-    double get_polar_radius_by_angle(double& fi) const{
-        if(cos(2*fi) > 0.0){
-            return sqrt(2*c*c*cos(2*fi));
+namespace LB{
+    class Lemniscate_Bernoulli{
+    private:
+        double c = 1.0;
+    public:
+        explicit Lemniscate_Bernoulli(double c){
+            this->c = c;
         }
-        else{
-            throw fi;
+        void set_data(double data_c){
+            c = data_c;
         }
-    }
-    double get_radius_of_curvature_by_polar_radius(double & r) const{
-        if(r > 0.0){
-            return 2*c*c/(3*r);
+        [[nodiscard]]
+        double get_data() const{
+            return c;
         }
-        else{
-            throw r;
-        };
-    }
-    double get_radius_of_curvature_by_angle(double & fi) const{
-        if(cos(2*fi) > 0.0){
-            return 2*c*c/(3 * get_polar_radius_by_angle(fi));
+        double get_polar_radius_by_angle(double& fi) const{
+            if(cos(2*fi) > 0.0)
+                return sqrt(2*c*c*cos(2*fi));
+            else
+                throw std::invalid_argument("Incorrect data");
         }
-        else{
-            throw fi;
+        double get_radius_of_curvature_by_polar_radius(double & r) const{
+            if(r > 0.0 && r < c*sqrt(2))
+                return 2*c*c/(3*r);
+            else
+                throw std::invalid_argument("Incorrect data");
         }
-    }
-    double get_sector_area_by_angle(double & alpha) const{
-        if(alpha >= 0 && alpha <= PI/4){
-            return c*c*sin(2*alpha)/2;
+        double get_radius_of_curvature_by_angle(double & fi) const{
+            if(cos(2*fi) > 0.0)
+                return 2*c*c/(3 * get_polar_radius_by_angle(fi));
+            else
+                throw std::invalid_argument("Incorrect data");
         }
-        else{
-            throw alpha;
+        double get_sector_area_by_angle(double & alpha) const{
+            if(alpha >= 0 && alpha <= PI/4)
+                return c*c*sin(2*alpha)/2;
+            else
+                throw std::invalid_argument("Incorrect data");
         }
-    }
-    [[nodiscard]]
-    double get_area_of_lemniscate() const{
-        return 2*c*c;
-    }
-};
+        [[nodiscard]]
+        double get_area_of_lemniscate() const{
+            return 2*c*c;
+        }
+    };
+}
+
+using namespace LB;
 
 int dialog(const char *msgs[], int);
 int D_Set_Data(Lemniscate_Bernoulli &);
